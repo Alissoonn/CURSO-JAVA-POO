a5f18b48af089a8f6060a68ceeb4aa004954cc13
Alisson Dias <alissondias1523@gmail.com>
2026-06-10 18:14:15 -0300

    att

---
+++ Curso_Java_POO/src/primeiroprograma/EstrutuRepeticao.java
@@ -0,0 +1,26 @@
+package primeiroprograma;
+
+public class EstrutuRepeticao {
+    public static void main(String[] args) {
+
+//-------------------------------------ESTRUTURA WHILE------------------------------------------------------
+       /*
+        int cc = 1;
+        while (cc < 12) {
+            cc = cc + 1;
+        if (cc==7){
+            break;
+        }
+            System.out.println("Cambalhota " + cc);
+        }
+        */
+
+//-------------------------------------ESTRUTURA DO------------------------------------------------------
+
+        int cc = 0;
+        do {
+            System.out.println(cc);
+            cc++;
+        }while (cc <= 5);
+    }
+   }
