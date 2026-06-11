b01d9896bbf5f0c13a7fae538c860ac8ddaca6e0
Alisson Dias <alissondias1523@gmail.com>
2026-06-11 18:06:57 -0300

    att

--- Curso_Java_POO/src/primeiroprograma/EstrutuRepeticao.java
+++ Curso_Java_POO/src/primeiroprograma/EstrutuRepeticao.java
@@ -6,7 +6,7 @@ public class EstrutuRepeticao {
 //-------------------------------------ESTRUTURA WHILE------------------------------------------------------
        /*
         int cc = 1;
-        while (cc < 12) {
+        while (cc < 12) { //Executa o bloco enquanto a condição for verdadeira
             cc = cc + 1;
         if (cc==7){
             break;
@@ -16,11 +16,25 @@ public class EstrutuRepeticao {
         */
 
 //-------------------------------------ESTRUTURA DO------------------------------------------------------
-
+      /*
         int cc = 0;
         do {
-            System.out.println(cc);
+            System.out.println(cc); //Código executa pelo menos uma vez, mesmo com a condição retorne falsa
             cc++;
         }while (cc <= 5);
-    }
-   }
+---------------------------------------------------------------------------------------------------------------
+        int x = 10;
+        do {
+           System.out.println("Executou"); //Código executa pelo menos uma vez, mesmo com a condição retorne falsa
+        } while (x < 5);
+
+       */
+//-------------------------------------ESTRUTURA FOR------------------------------------------------------
+
+        for(int cc = 100; cc >= 1; cc-=10){//no FOR a declaração da variavel deve ser dentro do laço
+            System.out.println(cc);// quase mesma coisa do WHILE mas deve ser feito quando sabe quantas repetições ira fazer
+        }
+
+
+  }
+}
