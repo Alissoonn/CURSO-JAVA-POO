d446ff3fbd21721dc7e7c4b10d677497bd5383fa
Alisson Dias <alissondias1523@gmail.com>
2026-08-03 16:36:51 -0300

    att
    revisão

--- Curso_Java_POO/src/primeiroprograma/EstrutuCondicional.java
+++ Curso_Java_POO/src/primeiroprograma/EstrutuCondicional.java
@@ -2,30 +2,30 @@ package primeiroprograma;
 import java.util.Scanner;
 
 
-public class EstrutuCondicional
-{
-    public static void main(String[] args)
-    {
+public class EstrutuCondicional {
+    public static void main(String[] args) {
 //-------------------------------Media de notas------------------------------------------
         /*
-        Scanner teclado = new Scanner(System.in);
+        Scanner teclado = new Scanner(System.in); //teclado é o nome da variavel que armazena o objeto SCANNER
         System.out.println("Digite o primeiro valor: ");
-        float n1 = teclado.nextFloat();
+        float n1 = teclado.nextFloat(); //teclado.nextFloat() converte o valor digitado para Float
         System.out.println("Digite o segundo valor: ");
         float n2 = teclado.nextFloat();
         float media = (n1 + n2) / 2;
         System.out.println("Media: " + media);
 
-        if(media >= 7){
+        if (media >= 7) {
             System.out.println("Você foi aprovado");
-        } else if (media >=5) {
+        } else if (media >= 5) {
             System.out.println("Esta de recuperação");
-        }else {
+        } else {
             System.out.println("Esta reprovado");
         }
-        */
+    }
+}
+*/
 //-------------------------------Maior/Menor de idade------------------------------------------
-        /*
+/*
         System.out.println("Em que ano voce nasceu?");
         Scanner teclado = new Scanner(System.in);
         int nasci = teclado.nextInt();
@@ -33,15 +33,16 @@ public class EstrutuCondicional
         int idade = ano - nasci;
         System.out.println("Sua idade é: " + idade);
 
-        if(ano - nasci >= 18)
-        {
+        if (ano - nasci >= 18) {
             System.out.println("Voce ja é maior de idade");
-        } else
-        {
+        } else {
             System.out.println("Voce é menor de idade");
         }
-        */
+    }
+}
+*/
 //-------------------------------Pode ou nao votar------------------------------------------
+
 
         System.out.println("Digite seu ano de nascimento: ");
         Scanner teclado  = new Scanner(System.in);
---
+++ Curso_Java_POO/src/primeiroprograma/EstruturaCondicional2.java
@@ -0,0 +1,142 @@
+package primeiroprograma;
+import java.util.Scanner;
+
+public class EstruturaCondicional2 {
+    public static void main(String[] args) {
+        /*
+        🟢 NIVEL 1 – if
+        Exercício 1 - Leia um número inteiro.
+        Se ele for positivo, imprima:
+        Número positivo */
+
+        /*
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um número: ");
+        int n1 = sc.nextInt();
+        System.out.println("Numero digitado foi: "+n1);
+
+        if(n1 > 0 ){
+            System.out.println("Numero Positivo");
+        }else {
+            System.out.println("Numero Negativo");
+        }*/
+//--------------------------------------------------------------------------------------------------------------------
+        /*
+        Exercício 2 - Leia a idade de uma pessoa.
+        Se ela tiver 18 anos ou mais, imprima:
+        Maior de idade      */
+
+        /*
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Qual sua idade? ");
+        int idade = sc.nextInt();
+
+        if(idade >= 18){
+            System.out.println("Você é maior de idade");
+        }else{
+            System.out.println("Menor de idade");
+        }*/
+//--------------------------------------------------------------------------------------------------------------
+        /*
+        Exercício 3 - Leia um número.
+        Se ele for igual a zero, imprima:
+        O número é zero                 */
+
+        /*
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um número ");
+        float n1 = sc.nextFloat();
+
+        if(n1 == 0 ){
+            System.out.println("O número é zero");
+        }else{
+            System.out.println("O numero não é zero");
+        }
+        */
+//--------------------------------------------------------------------------------------------------------------
+        /*
+        🟡 Nível 2 – if + else
+        Exercício 4
+        Leia um número.
+        Informe se ele é:
+        Positivo
+        Negativo
+        */
+
+        /*
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite qualquer número: ");
+        float n1 = sc.nextFloat();
+
+        if(n1 > 0){
+            System.out.println("Número positivo");
+        }else {
+            System.out.println("Numero negativo");
+        }*/
+//---------------------------------------------------------------------------------------------------------
+
+        /*
+        Exercício 5
+        Leia uma nota.
+        Se a nota for maior ou igual a 7:
+        Aprovado
+        Caso contrário:
+        Reprovado
+        */
+
+        /*
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Qual a nota do aluno? ");
+        int nota = sc.nextInt();
+
+        if(nota >= 7){
+            System.out.println("Aprovado");
+        }else{
+            System.out.println("Reprovado");
+        }
+         */
+//--------------------------------------------------------------------------------------------------------------
+
+        /*
+        Exercício 6
+        Leia dois números.
+        Informe qual deles é o maior.*/
+
+        /*
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Primeiro numero: ");
+        int primeiro = sc.nextInt();
+        System.out.println("Segundo numero: ");
+        int segundo = sc.nextInt();
+
+        if(primeiro > segundo){
+            System.out.println("Número "+primeiro+" é maior");
+        }else{
+            System.out.println("Número "+segundo+" é maior");
+        }
+         */
+//-------------------------------------------------------------------------------------------------------------
+
+        /*
+        🟠 Nível 3 – else if
+        Exercício 7
+        Leia uma nota.
+        Mostre:
+        Nota >= 9
+        Excelente
+        Nota >= 7
+        Bom
+        Nota >= 5
+        Recuperação
+        Menor que 5
+        Reprovado
+         */
+
+        
+
+
+
+
+
+    }
+}
